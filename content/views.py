24f3501b04d0567142d86e08d20e56d1bc835a62
kevinjared <kevinjaredkip@gmail.com>
2018-10-05 17:39:21 +0300

    modified views

--- content/views.py
+++ content/views.py
@@ -1,12 +1,18 @@
-from django.http  import HttpResponse
-from django.shortcuts import render
+from django.http  import HttpResponse, Http404,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import NewArticleForm
 import datetime as dt
 from .models import Image, Location, Category
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/') 
 def content_of_day(request):
     all_images = Image.objects.all()
     return render(request, 'all-images/content.html', {"images": all_images})
+
+def profile(request):
+    return render(request, 'all-images/profile.html', {"profile": profile})
 
 def convert_dates(dates):
 
@@ -46,3 +52,18 @@ def content(request,image_id):
     except DoesNotExist:
         raise Http404()
     return render(request,"all-images/article.html", {"content":content})(request, 'all-images/search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('contentToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
