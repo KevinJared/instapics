139a22a75bab742cd21bc58ceb0b0d3de100b1c7
kevinjared <kevinjaredkip@gmail.com>
2018-10-05 17:39:10 +0300

    modified urls

--- content/urls.py
+++ content/urls.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
 
 urlpatterns=[
     url(r'^$',views.content_of_day,name='contentToday'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^new/article$', views.new_article, name='new-article'),
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
