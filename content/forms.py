7737fd4111197e769cf955451258ef760a80ad9d
kevinjared <kevinjaredkip@gmail.com>
2018-10-07 17:17:56 +0300

    comment form

--- content/forms.py
+++ content/forms.py
@@ -11,8 +11,19 @@ class UserForm(forms.ModelForm):
         model = Profile
         fields = ('name','user_name','bio')
 
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget=forms.TextInput()
+        self.fields['text'].widget.attrs['placeholder']='Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
 
